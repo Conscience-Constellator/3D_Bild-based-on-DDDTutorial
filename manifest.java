b7a63ad10ced2dcd37ee61f1e63bb28737c8d09a
Conscience-Constellator <66545800+Conscience-Constellator@users.noreply.github.com>
2025-10-30 14:49:25 -0400

    Create manifest.java

---
+++ manifest.java
@@ -0,0 +1,8 @@
+{
+  "project_root": ".",
+  "modules": "SOrc",
+  "entry_points": "SOrc/MAn/MAn.java",
+  "priority": "",
+  "repo_urls": "https://github.com/Conscience-Constellator/3D_Bild-based-on-DDDTutorial",
+  "notes": ""
+}
